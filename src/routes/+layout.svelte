681f6d0a8a9c4afaf4bb1388e02960095a521e54
Script Raccoon <scriptraccoon@gmail.com>
2025-04-16 22:50:38 +0200

    link to homepage in nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
+	import Fa from 'svelte-fa'
+	import { faHome } from '@fortawesome/free-solid-svg-icons'
 	import './app.css'
 	let { children } = $props()
 </script>
 
 <div class="wrapper">
+	<nav>
+		<a href="/" aria-label="home">
+			<Fa icon={faHome} />
+		</a>
+	</nav>
+
 	{@render children()}
 </div>
+
+<style>
+	nav {
+		display: flex;
+		justify-content: end;
+		margin-bottom: -1rem;
+		font-size: 1rem;
+		padding-top: 0.25rem;
+
+		a {
+			color: black;
+		}
+	}
+</style>
