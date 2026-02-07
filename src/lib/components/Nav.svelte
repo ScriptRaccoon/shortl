ecd5b612ddd917450f76e76ff8dbdbe0b09592d0
Script Raccoon <scriptraccoon@gmail.com>
2026-02-07 21:12:11 +0100

    add github link

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
-	import { House } from 'lucide-svelte'
+	import { House, Github } from 'lucide-svelte'
 </script>
 
 <nav>
 	<a href="/" aria-label="home">
 		<House />
 	</a>
+	<a
+		href="https://github.com/ScriptRaccoon/shortl"
+		aria-label="GitHub repository"
+		target="_blank"
+	>
+		<Github />
+	</a>
 </nav>
 
 <style>
 	nav {
 		display: flex;
+		flex-direction: row-reverse;
 		justify-content: end;
 		margin-top: 1rem;
+		gap: 0.5rem;
 	}
 </style>
